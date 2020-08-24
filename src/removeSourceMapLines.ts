e75976e21c88a6575ce487bdfebcf2a98c17ce55
Kei Ito <kei.itof@gmail.com>
2020-08-24 19:09:15 +0900

    fix: update the regex pattern

--- src/removeSourceMapLines.ts
+++ src/removeSourceMapLines.ts
@@ -4,7 +4,7 @@ export const removeSourceMapLines = (
     const lines = input.split(/\r\n|\r|\n/);
     const lineNumberWidth = `${lines.length + 1}`.length;
     return lines.filter((line, index) => {
-        if ((/^\s*\/[/*]\s*#\s+sourceMappingURL\s*=/).test(line)) {
+        if ((/^\s*\/[/*]\s*#\s*sourceMappingURL\s*=/).test(line)) {
             const lineNumber = `${index + 1}`.padStart(lineNumberWidth);
             console.log(`  ${lineNumber}| ${line}`);
             return false;
